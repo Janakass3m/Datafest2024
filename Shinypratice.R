05909441b29a1949a1052618639bf6964e8b8de6
rsaquib1 <147536381+rsaquib1@users.noreply.github.com>
2024-04-13 16:48:40 -0500

    Add files via upload

---
+++ Shinypratice.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+ui <- fluidPage(
+  tags$video(src = "videos\\Purple animated tech festival promo instagram post.mp4", type = "video/mp4", controls = TRUE, width = "70%", height = "600px")
+)
+
+server <- function(input, output, session) {
+  addResourcePath(prefix = "videos", directoryPath = 'C:\\Users\\rsaqu\\Downloads')
+}
+
+shinyApp(ui, server)
+
+
