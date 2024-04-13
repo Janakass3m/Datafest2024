9de5514598a6bb488ac7bb2544cf01de7306b1f5
MJ <73719779+mj307@users.noreply.github.com>
2024-04-13 15:50:34 -0700

    Add files via upload

--- shinyCover.R
+++ shinyCover.R
@@ -2,19 +2,39 @@ library(shiny)
 
 ui <- fluidPage(
   tags$head(
+    # adjusts page to browser window
     tags$style(HTML("
-      video {
-        display: block;
-        margin: 0 auto;
+      #page-turn {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+      }
+      #page {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('page.png'); /* Replace 'page.png' with your page image */
+        background-size: cover;
+        transition: transform 1s ease-in-out;
+      }
+      .turning {
+        transform: rotateY(-180deg);
       }
     "))
   ),
-  tags$video(src = "videos\\Purple animated tech festival promo instagram post.mp4", type = "video/mp4", controls = TRUE, width = "70%", height = "600px")
+  div(id = "page-turn",
+      tags$video(src = "videos\\Purple animated tech festival promo instagram post.mp4", type = "video/mp4", controls = TRUE, width = "100%", height = "100%"),
+      div(id = "page")
+  )
 )
 
 server <- function(input, output, session) {
-  addResourcePath(prefix = "videos", directoryPath = "C:\\Users\\rsaqu\\Downloads")
+  addResourcePath(prefix = "videos", directoryPath = "C:/Users/rsaqu/Downloads")
 }
 
 shinyApp(ui, server)
+
 
