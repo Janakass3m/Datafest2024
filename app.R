42993d705a9b62503bdaf95a69ef395e4ad33b0e
swatimisra <147536383+swatimisra@users.noreply.github.com>
2024-04-14 10:51:20 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -48,12 +48,24 @@ ui <- fluidPage(
         box-sizing: border-box; /* Include padding in the box size */
         border: 2px solid black; /* Border */
       }
-      
-      .question-box {
-        margin-top: 20px;
+
+      .graph-image {
+        width: 47%; /* Adjust the width of each graph */
+        margin-right: 3%; /* Adjust the margin between graphs */
+        margin-bottom: 20px; /* Adjust the margin between rows */
+        float: left;
+      }
+
+      .graph-row::after {
+        content: '';
+        display: table;
+        clear: both;
       }
       
-            
+      .question {
+        font-size: 13px; 
+      }
+
     "))
   ),
   div(class = "container-fluid",
@@ -62,6 +74,22 @@ ui <- fluidPage(
           tags$video(src = "videos/Purple animated tech festival promo instagram post.mp4", type = "video/mp4", controls = TRUE),
           br(),
           actionButton("next_page", "Next Page", style = "margin-top: 20px;")
+      ),
+      div(id = "table_of_contents", style = "display:none;",
+          tags$img(src = "images/image.png", class = "custom-image", style="position:absolute; top:20px; right:20px;"),
+          div(class = "white-box",
+              h2("Table of Contents", style = "font-size: 48px;"),
+              p("Introduction.....", style = "font-size: 25px;"),
+              p("A brief description of the textbook and introducing imporvements to the original CourseKata", style = "font-size: 15px;"),# Adjust font size as needed
+              p("Graphs......", style = "font-size: 25px;"), 
+              p("Graphs that explain our analysis and display relationships", style = "font-size: 15px;"),# Adjust font size as needed
+              p("Sample Questions....", style = "font-size: 25px;"),
+              p("Examples of the type questions that is recommended for the textbook", style = "font-size: 15px;"),
+              p("Conclusion......", style = "font-size: 25px;"), 
+              p("Concluding our findings", style = "font-size: 15px;"),
+              actionButton("next_page0", "Next Page", style = "margin-top: 20px;"),
+              actionButton("back_page0", "Back", style = "margin-top: 20px;")
+          )
       ),
       div(id = "page2", style = "display:none;",
           tags$img(src = "images/image.png", class = "custom-image"),
@@ -76,108 +104,116 @@ ui <- fluidPage(
           tags$img(src = "images/image.png", class = "custom-image"),
           div(class = "white-box",
               h2("Graphs"),
-              p("Content of the Graphs page."),
-              actionButton("next_graphs_page", "Next Page", style = "margin-top: 20px;"),
-              actionButton("back_graphs_page", "Back", style = "margin-top: 20px;")
+              div(class = "graph-row",
+                  tags$img(src = "images/image1.jpeg", class = "graph-image"),
+                  tags$img(src = "images/image3.jpeg", class = "graph-image")
+              ),
+              div(class = "graph-row",
+                  tags$img(src = "images/image4.jpeg", class = "graph-image"),
+                  tags$img(src = "images/graph1.png", class = "graph-image")
+              ),
+              actionButton("next_page3", "Next Page", style = "margin-top: 20px;"),
+              actionButton("back_page2", "Back", style = "margin-top: 20px;")
           )
       ),
       div(id = "page3", style = "display:none;",
           tags$img(src = "images/image.png", class = "custom-image"),
           div(class = "white-box",
               h2("Sample Questions"),
-              p(""),
-              actionButton("next_page3", "Next Page", style = "margin-top: 20px; margin-bottom: 20px;"),
-              actionButton("back_page1", "Back", style = "margin-top: 20px;margin-bottom: 20px;"),
-              p("Match the left items with the corresponding right items:"),
-              div(class = "question-box",
-                  selectInput("left_items_page3", label = "Left Items:",
-                              choices = c("A", "B", "C", "D"),
-                              multiple = TRUE),
-                  selectInput("right_items_page3", label = "Right Items:",
-                              choices = c("1", "2", "3", "4"),
-                              multiple = TRUE)
-              ),
-              actionButton("check_answers_page3", "Check Answers", style = "margin-top: 20px;"),
+              p(class = "question", "Multiple Choice Question: Where are we right now?"),
+              radioButtons("location", label = NULL, choices = c("SMU", "Austin", "Houston", "New York"), inline = TRUE),
+              actionButton("check_answer", "Check Answer", style = "margin-top: 20px;"),
+              p(id = "answer_feedback", ""),
+              hr(),
+              h3(class = "question", "Short Answer Question:"),
+              p(class = "question", "What is the capital of Texas?"),
+              textInput("capital", label = NULL, placeholder = "Your answer..."),
+              actionButton("check_capital", "Check Answer", style = "margin-top: 20px;"),
+              p(id = "capital_feedback", ""),
+              hr(),
+              h3(class = "question", "Choice Matrix Question:"),
+              fluidRow(
+                column(6, 
+                       tags$div(id = "row_13.5", style = "margin-bottom: 10px;",
+                                h4("13.5"),
+                                actionButton("13.5_value", label = "Value", style = "margin-right: 10px;"),
+                                actionButton("13.5_variable", label = "Variable", style = "margin-right: 10px;"),
+                                actionButton("13.5_neither", label = "Neither", style = "margin-right: 10px;")
+                       )
+                ),
+                column(6, 
+                       tags$div(id = "row_Texas", style = "margin-bottom: 10px;",
+                                h4("Texas"),
+                                actionButton("Texas_value", label = "Value", style = "margin-right: 10px;"),
+                                actionButton("Texas_variable", label = "Variable", style = "margin-right: 10px;"),
+                                actionButton("Texas_neither", label = "Neither", style = "margin-right: 10px;")
+                       )
+                )
+              )
+          ),
+              actionButton("next_page4", "Next Page", style = "margin-top: 20px;"),
+              actionButton("back_graphs_page", "Back", style = "margin-top: 20px;")
           )
       ),
       div(id = "page4", style = "display:none;",
           tags$img(src = "images/image.png", class = "custom-image"),
           div(class = "white-box",
               h2("Conclusion"),
-              p(""),
+              p("From our analysis, we have concluded that a more diverse and variable type of questions in an interactive textbook would engage students more. As seen previously, students generally performed worse when they had a large number of the same number of questions (ex. MCQ) compared to when they had multiple different types of questions. These types of questions would hone in the student’s attention and help them practice different skills when it comes to understanding the content. Chapters in this interactive textbook such as 12, 10, and 7 were also seen to have the worst performance in the book. 
+"),
               actionButton("back_page3", "Back", style = "margin-top: 20px;")
           )
       )
-  ),
+  )
   tags$script("
     $(document).ready(function() {
       $('#next_page').click(function() {
         $('#page1').hide();
+        $('#table_of_contents').show();
+      });
+      $('#back_page0').click(function() {
+        $('#table_of_contents').hide();
+        $('#page1').show();
+      });
+
+      $('#next_page0').click(function() {
+        $('#table_of_contents').hide();
         $('#page2').show();
       });
       $('#back_page2').click(function() {
         $('#page2').hide();
-        $('#page1').show();
+        $('#table_of_contents').show();
       });
 
       $('#next_page2').click(function() {
         $('#page2').hide();
         $('#graphs_page').show();
       });
-      $('#back_graphs_page').click(function() {
+      $('#back_page2').click(function() {
+        $('#page2').hide();
+        $('#page1').show();
+      });
+
+      $('#next_page3').click(function() {
+        $('#graphs_page').hide();
+        $('#page3').show();
+      });
+      $('#back_page3').click(function() {
         $('#graphs_page').hide();
         $('#page2').show();
       });
 
-      $('#next_graphs_page').click(function() {
-        $('#graphs_page').hide();
-        $('#page3').show();
-      });
-      $('#back_page1').click(function() {
-        $('#page3').hide();
-        $('#graphs_page').show();
-      });
-
-      $('#next_page3').click(function() {
+      $('#next_page4').click(function() {
         $('#page3').hide();
         $('#page4').show();
       });
-
-      $('#back_page3').click(function() {
+      $('#back_page4').click(function() {
         $('#page4').hide();
         $('#page3').show();
       });
     });
   ")
-)
-
-server <- function(input, output) {
-  observeEvent(input$check_answers_page3, {
-    left_selected <- input$left_items_page3
-    right_selected <- input$right_items_page3
-    
-    # Define the correct matching pairs for page 3
-    correct_pairs_page3 <- list(A = "1", B = "2", C = "3", D = "4")
-    
-    # Check if all selections on page 3 are correct
-    if (identical(sort(left_selected), names(correct_pairs_page3)) &&
-        identical(sort(right_selected), unlist(correct_pairs_page3))) {
-      # Display modal dialog for correct answers on page 3
-      showModal(modalDialog(
-        title = "Results",
-        "Correct! All matches on Page 3 are right."
-      ))
-    } else {
-      # Display modal dialog for incorrect answers on page 3
-      showModal(modalDialog(
-        title = "Results",
-        "Incorrect! Some matches on Page 3 are wrong. Please try again."
-      ))
-    }
-  })
-}
-
-
 
 shinyApp(ui, server = function(input, output) {})
+
 
